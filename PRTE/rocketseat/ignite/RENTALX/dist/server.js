852b175236e2e79e08275ea8ae0b84914b459d99
Salmi Nunes <salminunes@gmail.com>
2022-04-11 12:23:39 -0300

    Fim cap II

--- PRTE/rocketseat/ignite/RENTALX/dist/server.js
+++ PRTE/rocketseat/ignite/RENTALX/dist/server.js
@@ -4,5 +4,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const categories_routes_1 = require("./routes/categories.routes");
 const app = (0, express_1.default)();
-app.listen(3333);
+app.use(express_1.default.json());
+app.use('/categories', categories_routes_1.categoriesRoutes);
+app.listen(3333, () => console.log('Server is running!'));
